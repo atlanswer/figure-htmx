fa69c03246dbbbce4e98c7228dd346dbffd50207
Atlanswer <atlanswer@gmail.com>
2023-09-06 17:57:37 +0800

    switch to static files for caching

--- api/main.py
+++ api/main.py
@@ -1,19 +1,10 @@
 from typing import Union
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/public", StaticFiles(directory="public"), name="Public")
 
 templates = Jinja2Templates(directory="templates")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
 
 @app.get("/items/{item_id}")
 async def get_item(item_id: int, q: Union[str, None] = None):
