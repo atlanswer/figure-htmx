23b35d9cf6428d1a971e53ddde610bb2f7d04dac
Atlanswer <atlanswer@gmail.com>
2023-09-06 12:30:32 +0800

    add static htmx and unocss

--- api/main.py
+++ api/main.py
@@ -1,12 +1,18 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/public", StaticFiles(directory="public"), name="Public")
+
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def get_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
