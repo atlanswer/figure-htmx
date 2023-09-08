5d2147861201a3ed80af488fc6c909c1f867e896
Atlanswer <i@atlanswer.com>
2023-09-08 15:29:35 +0800

    add dev server

--- api/dev.py
+++ api/dev.py
@@ -1,8 +1,11 @@
+from fastapi.exceptions import StarletteHTTPException
 from typing import Awaitable, Callable
-from fastapi import FastAPI, Request, Response
-from fastapi.staticfiles import StaticFiles
+import typing
 
-app = FastAPI()
+from fastapi import Request, Response
+from fastapi.staticfiles import StaticFiles
+from starlette.types import Message
+from main import app
 
 app_static = StaticFiles(directory="public", html=True)
 
@@ -11,5 +14,20 @@ app_static = StaticFiles(directory="public", html=True)
 async def redirect_static_file(
     req: Request, call_next: Callable[[Request], Awaitable[Response]]
 ):
-    res = await call_next(req)
+    res = Response()
+
+    async def send(msg: Message) -> typing.NoReturn:
+        if "status" in msg:
+            res.status_code = msg["status"]
+        if "headers" in msg:
+            res.raw_headers = msg["headers"]
+        res.body += msg.get("body", b"")
+
+    try:
+        await app_static(req.scope, req.receive, send)
+    except StarletteHTTPException as e:
+        if e.status_code != 404:
+            raise e
+        res = await call_next(req)
+
     return res
