5de62d8bbfd7cff6432e37e10645b9c16bba446a
Atlanswer <i@atlanswer.com>
2023-09-08 15:33:36 +0800

    format files

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 from typing import Union
+
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/items/{item_id}")
 async def get_item(item_id: int, q: Union[str, None] = None):
